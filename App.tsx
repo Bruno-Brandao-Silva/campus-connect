a76f865e65a88f13cf6440dd61e76a50e9c52aaf
Victor <victorbusolin@alunos.utfpr.edu.br>
2023-11-17 18:13:16 -0300

    feat: included native base provider on app

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import { useFonts } from 'expo-font';
 import { Text, View, StatusBar } from 'react-native';
 import { Montserrat_400Regular, Montserrat_600SemiBold, Montserrat_700Bold } from '@expo-google-fonts/montserrat';
 import { OpenSans_600SemiBold } from '@expo-google-fonts/open-sans';
+import { NativeBaseProvider } from 'native-base';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -9,10 +10,12 @@ export default function App() {
   })
 
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <StatusBar barStyle={'light-content'} translucent backgroundColor="transparent" />
-      {fontsLoaded ? <Text>Hello world</Text> : <View />}
-    </View>
+    <NativeBaseProvider>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <StatusBar barStyle={'light-content'} translucent backgroundColor="transparent" />
+        {fontsLoaded ? <Text>Hello world</Text> : <View />}
+      </View>
+    </NativeBaseProvider>
   );
 }
 
