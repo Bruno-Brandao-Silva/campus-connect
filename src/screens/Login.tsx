4330f3293d6a1e8706932a5bf011e4fdb7318c43
Victor <victorbusolin@alunos.utfpr.edu.br>
2023-11-18 15:56:28 -0300

    feat: login screen

---
+++ src/screens/Login.tsx
@@ -0,0 +1,38 @@
+import { Button } from "@components/Button";
+import { Input } from "@components/Input";
+import { Center, Heading, Image, ScrollView, Text, VStack, View } from "native-base";
+import CAMPUSCONNECT from "../../assets/CAMPUSCONNECT.png";
+
+export function Login() {
+  return (
+    <ScrollView contentContainerStyle={{ flexGrow: 1 }} showsVerticalScrollIndicator={false}>
+      <VStack flex={1} bg={"green.100"}>
+        <Image
+          source={CAMPUSCONNECT}
+          alt="CAMPUSCONNECT"
+          mt={24}
+          w={'4/5'}
+          resizeMode="center"
+          alignSelf="center"
+        />
+
+        <Center mt={16} flex={1} px={8} display={'flex'}>
+          <View w={"full"}>
+            <Text color={"yellow.100"} fontFamily={"body"} fontSize={"xl"}>Login</Text>
+            <Input placeholder="Identificação de usuário (R.A.)" keyboardType="number-pad" my={4} />
+          </View>
+
+          <View w={"full"}>
+            <Text color={"yellow.100"} fontFamily={"body"} fontSize={"xl"}>Senha</Text>
+            <Input placeholder="Senha" secureTextEntry my={4} />
+          </View>
+          <Button title="Acessar conta" variant="solid" my={4} />
+        </Center>
+
+        <Text my={12} color={'yellow.100'} mx={8} textAlign={'center'} fontSize={'md'}>
+          Somente para alunos da UTFPR
+        </Text>
+      </VStack>
+    </ScrollView>
+  )
+}
