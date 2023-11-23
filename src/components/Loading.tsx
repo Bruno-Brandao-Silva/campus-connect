a7fc9ff66803acde66b5e71a446333dcbf14b0b1
Victor <victorbusolin@alunos.utfpr.edu.br>
2023-11-23 10:48:31 -0300

    fix: loading bg

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -2,7 +2,7 @@ import { Center, Spinner } from "native-base";
 
 export function Loading() {
   return (
-    <Center flex={1} bg={"purple.900"}>
+    <Center flex={1} bg={"green.100"}>
       <Spinner color={"orange.600"} size={"lg"} />
     </Center>
   )
