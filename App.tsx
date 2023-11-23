785dc0abcb080a100cf1c97599156207210e53b4
Bruno Brandão Silva <brunowbrandao@gmail.com>
2023-11-22 21:29:41 -0300

    Refactor App component and remove unused imports

--- App.tsx
+++ App.tsx
@@ -1,52 +1,19 @@
-import { useFonts } from 'expo-font';
 import { StatusBar } from 'react-native';
-import { Montserrat_400Regular, Montserrat_600SemiBold, Montserrat_700Bold } from '@expo-google-fonts/montserrat';
-import { OpenSans_600SemiBold } from '@expo-google-fonts/open-sans';
 import { NativeBaseProvider } from 'native-base';
-import { Loading } from '@components/Loading';
 import { THEME } from './src/theme';
-import { Login } from '@screens/Login';
-import { Onboarding } from '@screens/Onboarding';
-import { AuthProvider, useAuth } from '@contexts/AuthContext';
+import { AuthProvider } from '@contexts/AuthContext';
 import axios from 'axios';
-import { PostForm } from './src/screens/PostForm';
-import { Feed } from '@screens/Feed';
-import { Profile } from '@screens/Profie';
+import { Main } from '@screens/Main';
 
 axios.defaults.baseURL = process.env.EXPO_PUBLIC_API_URL;
 
 export default function App() {
   return (
     <AuthProvider>
-      <Layout />
+      <NativeBaseProvider theme={THEME}>
+        <StatusBar barStyle={'light-content'} translucent backgroundColor="transparent" />
+        <Main/>
+      </NativeBaseProvider>
     </AuthProvider>
   );
 }
-
-const Layout = () => {
-  let opc = 2;
-  const { authState } = useAuth();
-  const [fontsLoaded] = useFonts({
-    Montserrat_400Regular, Montserrat_600SemiBold, Montserrat_700Bold, OpenSans_600SemiBold
-  })
-  return (
-    <NativeBaseProvider theme={THEME}>
-      <StatusBar barStyle={'light-content'} translucent backgroundColor="transparent" />
-      {fontsLoaded ?
-        (authState.authenticated ?
-          (authState.firstAccess ?
-            <Onboarding /> :
-            (() => {
-              switch (opc) {
-                case 0: return <Feed />;
-                case 1: return <PostForm />;
-                case 2: return <Profile />;
-              }
-            })()) :
-          <Login />) :
-        <Loading />}
-    </NativeBaseProvider>
-  )
-}
-
-
